15255af39efe9265e487972d2aa7b9125fd0ac7b
Dominik Flisak <d.flisak@gmail.com>
2020-04-05 10:52:45 +0200

    build: add network config and setup rinkeby

---
+++ networks.js
@@ -0,0 +1,20 @@
+const { provider, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+    networks: {
+        development: {
+            protocol: 'http',
+            host: 'localhost',
+            port: 8545,
+            gas: 5000000,
+            gasPrice: 5e9,
+            networkId: '*',
+        },
+        rinkeby: {
+            provider: () => new HDWalletProvider(mnemonic, provider),
+            networkId: 4,
+            gasPrice: 10e9
+        }
+    },
+};
